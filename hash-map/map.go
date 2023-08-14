113eb6b7b5295dbd284150f6f8ebe0cb45be7fc8
Noush-012 <noushadibrahim012@gmail.com>
2023-08-14 10:55:32 +0530

    addons

---
+++ hash-map/map.go
@@ -0,0 +1,17 @@
+package hashmap
+
+// func Mapping() {
+
+// 	mapp := make(map[string]int)
+
+// 	for i := 'a'; i < 'j'; i++ {
+// 		mapp[string(i)]++
+// 	}
+
+// 	// fmt.Println(mapp)
+
+// 	for k, v := range mapp {
+
+// 	}
+
+// }
--- main.go
+++ main.go
@@ -1,7 +1,8 @@
 package main
 
 import (
-	"sammple/api"
+	"fmt"
+	"runtime"
 )
 
 //           ----------           To Do              ------------                         //
@@ -31,6 +32,71 @@ func main() {
 
 	// fmt.Println(t1, t2)
 
-	api.GetAllCarMakes()
+	// api.GetAllCarMakes()
+	// hashmap.Mapping()
 
+	// c := Closure()
+	// fmt.Println(c(5))
+	// fmt.Println(c(5))
+	// fmt.Println(c(5))
+
+	// i := new(person)
+	// i.name = "John"
+
+	// ManualGarbageCollection()
+
+	// arr := []int{2, 6, 89, 8, -8, 7, 6, 9, 25, 1, 3}
+	// sort.Ints(arr)
+	// fmt.Println(arr)
+	fib := FibClosure()
+	for i := 0; i < 10; i++ {
+		fmt.Println(fib())
+	}
+
+}
+
+func FibClosure() func() int {
+	n1 := 0
+	n2 := 1
+
+	return func() int {
+
+		t3 := n1
+		n1, n2 = n2, n1+n2
+
+		return t3
+	}
+}
+
+type Person struct {
+	Name string
+}
+
+func CreatePerson() *Person {
+	p := &Person{
+		Name: "Anas",
+	}
+
+	if p.Name == "" {
+		return &Person{}
+	}
+	return nil
+}
+
+func ManualGarbageCollection() {
+	// Allocate memory using the runtime package
+	mem := runtime.MemStats{}
+	runtime.ReadMemStats(&mem)
+	fmt.Printf("Initial memory allocated: %d bytes\n", mem.Alloc)
+
+	arr := []int{1, 5, 55, 216, 16, 5}
+
+	arr = append(arr, 1)
+
+	// Deallocate memory manually
+	runtime.GC()
+
+	// Check memory after garbage collection
+	runtime.ReadMemStats(&mem)
+	fmt.Printf("Memory allocated after GC: %d bytes\n", mem.Alloc)
 }
